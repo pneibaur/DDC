b81a62b57d1f47b179ae93b88bf3752dd8c6201e
Phil Neibaur <pneibaur+github@gmail.com>
2023-05-18 20:40:58 -0600

    WIP replaces react bootstrap form tags

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Form, Row, Col, Button, Container } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 
 const ContactForm = () => {
     return (
@@ -10,42 +10,42 @@ const ContactForm = () => {
                 </Row>
                 <hr /><br />
                 <Row>
-                    <form 
-                    method='POST' 
-                    name='contactDrew' 
-                    data-netlify="true" 
-                    netlify-honeypot="bot-field"
-                    data-netlify-recaptcha="true"
+                    <form
+                        method='POST'
+                        name='contactDrew'
+                        data-netlify="true"
+                        netlify-honeypot="bot-field"
+                        data-netlify-recaptcha="true"
                     >
-                        <input type="hidden" name='bot-field'/>
+                        <input type="hidden" name='bot-field' />
                         <input type="hidden" name='form-name' value="contactDrew" />
                         <input type="hidden" name='subject' value='inquiry from DrewDavisCounseling.com' />
                         <Row>
                             <Col>
-                                <Form.Group className='contactForm'>
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control placeholder='First & Last Name' type='text' className='contactForm' />
-                                </Form.Group>
+                                <div classname="Form-Group">
+                                    <label htmlFor='name' >Name</label>
+                                    <input type="name" placeholder='First & Last Name' className='contactForm form-control' />
+                                </div>
                             </Col>
                             <Col>
-                                <Form.Group className='contactForm'>
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control placeholder='someone@example.com' type='email' className='contactForm' />
-                                </Form.Group>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" name="email" id="email" className="form-control contactForm" placeholder='someone@example.com' />
+                                </div>
                             </Col>
                         </Row> <br />
                         <Row>
                             <Col>
-                                <Form.Group className='contactForm'>
-                                    <Form.Label>Tell me how I can help:</Form.Label>
-                                    <Form.Control placeholder='Your message here' as='textarea' rows={3} className='contactForm' />
-                                </Form.Group>
+                            <div className="form-group">
+                                <label htmlFor="inquiryMessage">Tell me how I can help:</label>
+                                <textarea name="inquiryMessage" id="inquiryMessage" className='form-control contactForm' rows="3" placeholder='Do not include any personal or sensitive information.'></textarea>
+                            </div>
                             </Col>
                         </Row> <br />
                         <Row className='justify-content-md-start'>
                             <Col xs={2}>
                                 <div data-netlify-recaptcha="true"></div>
-                                <Button variant='info' type='submit'>Submit</Button>
+                                <button className='btn btn-primary' type='submit'>Submit</button>
                             </Col>
                         </Row>
                         <br />
