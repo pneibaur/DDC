e2743da8f63040000ef4ae86382d1dfb7c7bcbf0
Phil Neibaur <pneibaur+github@gmail.com>
2023-05-19 13:32:54 -0600

    adds AJAX FNs for form submission

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,7 +1,34 @@
+import { navigate } from 'gatsby';
 import * as React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 
 const ContactForm = () => {
+
+    const [newForm, setNewForm] = React.useState({
+        name: "",
+        email: "",
+        inquiryMessage: "",
+    });
+
+    const handleChange = (event) => {
+        setNewForm({ ...newForm, [event.target.name]: event.target.value})
+        console.log(setNewForm)
+    };
+
+    const handleSubmit = (event) => {
+        event.preventdefault();
+
+        const contactForm = event.target;
+        const formData = new FormData(contactForm);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        }).then(() => navigate("/thank-you")).catch((error) => alert(error));
+    };
+
+
     return (
         <div className='contactFormDiv'>
             <Container>
@@ -16,21 +43,22 @@ const ContactForm = () => {
                         data-netlify="true"
                         // netlify-honeypot="bot-field"
                         data-netlify-recaptcha="true"
+                        onSubmit={handleSubmit}
                     >
                         <input type="hidden" name='bot-field' />
                         <input type="hidden" name='form-name' value="contactDrew" />
                         <input type="hidden" name='subject' value='inquiry from DrewDavisCounseling.com' />
                         <Row>
                             <Col>
-                                <div classname="Form-Group">
+                                <div className="Form-Group">
                                     <label htmlFor='name' >Name</label>
-                                    <input type="text" id='name' name='name' placeholder='First & Last Name' className='contactForm form-control' />
+                                    <input type="text" id='name' name='name' placeholder='First & Last Name' className='contactForm form-control' onSubmit={handleChange}/>
                                 </div>
                             </Col>
                             <Col>
                                 <div className="form-group">
                                     <label htmlFor="email">Email</label>
-                                    <input type="email" name="email" id="email" className="form-control contactForm" placeholder='someone@example.com' />
+                                    <input type="email" name="email" id="email" className="form-control contactForm" placeholder='someone@example.com' onSubmit={handleChange} />
                                 </div>
                             </Col>
                         </Row> <br />
@@ -38,7 +66,7 @@ const ContactForm = () => {
                             <Col>
                             <div className="form-group">
                                 <label htmlFor="inquiryMessage">Tell me how I can help:</label>
-                                <textarea name="inquiryMessage" id="inquiryMessage" className='form-control contactForm' rows="3" placeholder='Do not include any personal or sensitive information.'></textarea>
+                                <textarea name="inquiryMessage" id="inquiryMessage" className='form-control contactForm' rows="3" placeholder='Do not include any personal or sensitive information.' onSubmit={handleChange} ></textarea>
                             </div>
                             </Col>
                         </Row> <br />
