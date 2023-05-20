1ec806846da0f37b51df0c2f05940b308755dd5b
Phil Neibaur <pneibaur+github@gmail.com>
2023-05-20 08:38:26 -0600

    removes AJAX code

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,32 +1,32 @@
-import { navigate } from 'gatsby';
 import * as React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 
 const ContactForm = () => {
 
-    const [newForm, setNewForm] = React.useState({
-        name: "",
-        email: "",
-        inquiryMessage: "",
-    });
+    // const [newForm, setNewForm] = React.useState({
+    //     name: "",
+    //     email: "",
+    //     inquiryMessage: "",
+    // });
 
-    const handleChange = (event) => {
-        setNewForm({ ...newForm, [event.target.name]: event.target.value})
-        console.log(setNewForm)
-    };
+    // const handleChange = (event) => {
+    //     setNewForm({ ...newForm, [event.target.name]: event.target.value})
+    // };
 
-    const handleSubmit = (event) => {
-        event.preventdefault();
+    // const handleSubmit = (event) => {
+    //     alert("A value was submitted: " + this.state.value)
+    //     event.preventdefault();
 
-        const contactForm = event.target;
-        const formData = new FormData(contactForm);
+    //     const contactForm = event.target;
+    //     const formData = new FormData(contactForm);
 
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString(),
-        }).then(() => navigate("/thank-you")).catch((error) => alert(error));
-    };
+    //     fetch("/", {
+    //         method: "POST",
+    //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //         body: new URLSearchParams(formData).toString(),
+    //     }).then(() => navigate("/thank-you")).catch((error) => alert(error));
+    //     console.log("this is event: ", event)
+    // };
 
 
     return (
@@ -38,27 +38,27 @@ const ContactForm = () => {
                 <hr /><br />
                 <Row>
                     <form
-                        method='POST'
-                        name='contactDrew'
+                        method="POST"
+                        name="contactDrew"
                         data-netlify="true"
                         // netlify-honeypot="bot-field"
                         data-netlify-recaptcha="true"
-                        onSubmit={handleSubmit}
+                        action="/thank-you"
                     >
-                        <input type="hidden" name='bot-field' />
-                        <input type="hidden" name='form-name' value="contactDrew" />
-                        <input type="hidden" name='subject' value='inquiry from DrewDavisCounseling.com' />
+                        <input type="hidden" name="bot-field" />
+                        <input type="hidden" name="form-name" value="contactDrew" />
+                        <input type="hidden" name="subject" value="inquiry from DrewDavisCounseling.com" />
                         <Row>
                             <Col>
                                 <div className="Form-Group">
-                                    <label htmlFor='name' >Name</label>
-                                    <input type="text" id='name' name='name' placeholder='First & Last Name' className='contactForm form-control' onSubmit={handleChange}/>
+                                    <label htmlFor="firstLast" >Name</label>
+                                    <input type="text" id="firstLast" name="firstLast" placeholder="First & Last Name" className="contactForm form-control" />
                                 </div>
                             </Col>
                             <Col>
                                 <div className="form-group">
                                     <label htmlFor="email">Email</label>
-                                    <input type="email" name="email" id="email" className="form-control contactForm" placeholder='someone@example.com' onSubmit={handleChange} />
+                                    <input type="email" name="email" id="email" className="form-control contactForm" placeholder="someone@example.com" />
                                 </div>
                             </Col>
                         </Row> <br />
@@ -66,13 +66,13 @@ const ContactForm = () => {
                             <Col>
                             <div className="form-group">
                                 <label htmlFor="inquiryMessage">Tell me how I can help:</label>
-                                <textarea name="inquiryMessage" id="inquiryMessage" className='form-control contactForm' rows="3" placeholder='Do not include any personal or sensitive information.' onSubmit={handleChange} ></textarea>
+                                <textarea name="inquiryMessage" id="inquiryMessage" className="form-control contactForm" rows="3" placeholder="Do not include any personal or sensitive information." ></textarea>
                             </div>
                             </Col>
                         </Row> <br />
-                        <Row className='justify-content-md-start'>
+                        <Row className="justify-content-md-start">
                             <Col xs={2}>
-                                <button className='btn btn-primary' type='submit'>Submit</button>
+                                <button className="btn btn-primary" type="submit">Submit</button>
                             </Col>
                             <Col xs={2}>
                                 <div data-netlify-recaptcha="true"></div>
