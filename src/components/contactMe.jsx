3cddf1e27d93fd790e5646ff4cba183ebe76a07c
Phil Neibaur <pneibaur+github@gmail.com>
2023-08-26 20:13:59 -0600

    adds h1 site title

--- src/components/contactMe.jsx
+++ src/components/contactMe.jsx
@@ -11,20 +11,24 @@ const ContactMe = () => {
                 <hr /><br />
                 <Row className='contactMeRow'>
                 </Row>
-                    <ul className='contactMeList'>
-                        <li>
-                            <span className="material-symbols-rounded icons">mail</span> <i> Drew.Davis.Counseling@gmail.com</i>
-                        </li>
-                        <li>
-                            <span className="material-symbols-rounded icons">call</span> 801-810-5214
-                        </li>
-                        <li>
-                            <span className="material-symbols-rounded icons">pin_drop</span> 870 West Center St., Orem, UT 84057
-                        </li>
-                    </ul>
+                <ul className='contactMeList'>
+                    <li>
+                        <span className="material-symbols-rounded icons">mail</span> <i> Drew.Davis.Counseling@gmail.com</i>
+                    </li>
+                    <li>
+                        <span className="material-symbols-rounded icons">call</span> 801-810-5214
+                    </li>
+                    <li>
+                        <span className="material-symbols-rounded icons">pin_drop</span> 870 West Center St., Orem, UT 84057
+                    </li>
+                </ul>
                 <Row>
                     <hr />
-                    <p>&copy; Drew Davis Counseling | All rights reserved</p>
+                    <div>
+                        <span>
+                            &copy; <h1 className='siteTitle'>Drew Davis Counseling</h1> | All rights reserved
+                        </span>
+                    </div>
                 </Row>
             </Container>
         </div>
